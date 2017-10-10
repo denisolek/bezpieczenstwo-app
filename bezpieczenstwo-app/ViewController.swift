f84d676c4492855462e01763bb71c3c4f4bc2291
denisolek <denis.olek@gmail.com>
2017-10-10 16:42:55 +0200

    init

---
+++ bezpieczenstwo-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bezpieczenstwo-app
+//
+//  Created by Denis Olek on 10.10.2017.
+//  Copyright © 2017 Denis Olek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
